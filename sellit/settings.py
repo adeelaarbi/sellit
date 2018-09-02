d309fd33790437bf1da6ed234ab8b4e3070adaab
adeel <adeel.aarbi92@gmail.com>
2018-09-02 09:27:16 +0500

    .

--- sellit/settings.py
+++ sellit/settings.py
@@ -23,14 +23,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7$7$xi+!z36l3*+ipn@s^#nt20-@-b72&v($!_6v#aw#4-%h0s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["aiwah.pk", "127.0.0.1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
-    'app.apps.AppConfig',
-    'bootstrap3'
 ]
 
 SITE_ID = 1
@@ -82,8 +81,11 @@ WSGI_APPLICATION = 'sellit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'HOST': '',
+        'PORT': 5432,
     }
 }
 
@@ -125,5 +127,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "app/static")
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+#
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, "static"),
+#     ]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
